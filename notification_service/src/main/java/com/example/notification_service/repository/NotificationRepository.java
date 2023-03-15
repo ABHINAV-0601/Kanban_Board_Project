1d10e2a205328944591733591054024e7a221729
Abhinav Pandey <pandeyabhinav2608@gmail.com>
2023-03-15 15:30:25 +0530

    last commit

--- notification_service/src/main/java/com/example/notification_service/repository/NotificationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.notification_service.repository;
-
-import com.example.notification_service.domain.Notification;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface NotificationRepository extends MongoRepository<Notification,String> {
-
-}
--- user_service/src/main/java/com/niit/user_service/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.niit.user_service.service;
-
-import com.niit.user_service.domain.User;
-import com.niit.user_service.exception.UserAlreadyExistsException;
-
-public interface UserService {
-    User saveUser(User user) throws UserAlreadyExistsException;
-}
